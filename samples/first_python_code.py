0c16bc43d32422c8977f3e0605b7433bfbfe8c01
Coding Skull <neonblueflame@gmail.com>
2018-06-09 14:05:09 +0800

    Edited exercise 1

--- samples/first_python_code.py
+++ samples/first_python_code.py
@@ -2,7 +2,7 @@
 # text using different approach in
 # Python.
 
-organization = 'WomenWhoCode!'
+organization = 'Dark side!'
 
 # You can print it right away!
 print('Hello WomanWhoCode!')
@@ -15,7 +15,18 @@ print('Hello {}'.format(organization))
 # print(f'Hello {organization}')  # Can use this format with Python3.6
 
 # First, replace the following text with your name
-your_name = '<place your name here>'
+your_name = 'Darth Hanger'
 
 # Then, uncomment the next line by removing the "#" sign
-#print('{} loves Python!'.format(your_name))
+print('{} loves Python!'.format(your_name))
+
+string = "Hanger"
+print string
+print len(string)
+print string.upper()
+print string.upper().isupper()
+print string.lower()
+print string.lower().islower()
+print string[0]
+string[0] = "P"
+print string
